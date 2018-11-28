1c95282efc968039196d37604cfe8eb50f12f66e
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-11-28 19:31:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.on("message", (message) => {
  
   if (message.content.startsWith(prefix + "red")) {
     message.delete(1);
-    message.channel.send("Figyelem Trainerek! @everyone @here" + message.content);
+    message.channel.send(message.content.split(" ") + " Trainerek figyelem! @everyone @here");
   } 
 });
 
