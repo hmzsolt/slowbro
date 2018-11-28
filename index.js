b96aa0ad81dd840aaa930828d8aa23d3ec753bdf
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-11-28 13:45:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,17 +32,4 @@ var response = helloResponses[Math.floor(Math.random()*helloResponses.length)];
   }
 });
 
-let prefix = "!";
-client.on("message", (message) => {
-  // Exit and stop if the prefix is not there or if user is a bot
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
- 
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "foo")) {
-    message.channel.send("bar!");
-  }
-});
-
 client.login(process.env.token);
