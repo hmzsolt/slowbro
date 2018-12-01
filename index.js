570e429267da598d010317bcd64f6d8326990474
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-01 16:00:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ client.on("message", (message) => {
     message.channel.send(message.content.slice(5));
   } 
 	
-  if (msg.attachments.size > 0 && msg.channel.id === '458620540555493376')   {
+  if (message.attachments.size > 0 && message.channel.id === '458620540555493376')   {
     message.delete(1);
     message.channel.send("Kérlek, szöveges formátumban jelentsd. Pl.: !red GYM neve - idő - szint");
   }	
