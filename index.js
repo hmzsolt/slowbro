8cccb0e56fc780f581981af94634eca04eac1014
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-02 20:08:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ client.on("message", (message) => {
     message.delete();
     message.channel.send(" Trainerek figyelem! @everyone @here ");
     message.channel.send(message.content.slice(5));
-    message.react('👍').then(console.log).catch(console.error);
+    message.react(':thumbsup:').then(console.log).catch(console.error);
     
   
     } 
