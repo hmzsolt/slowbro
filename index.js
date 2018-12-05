926f5cb71c515b99d33d0931d3f5a143423e7a14
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-05 13:14:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ client.on("messageReactionAdd", (messageReaction, user, message) => {
   if(messageReaction.emoji.name === "👍") {
 
 console.log(messageReaction.count);
-	  return message.send(messageReaction.count);
+	  
   }
 });
 
