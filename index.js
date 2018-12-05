e490ca11d93a41365892961cf0de29f25d3c637c
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-05 12:15:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ client.on("message", (message) => {
 	
 });
 
-client.on("messageReactionAdd", (messageReaction) => console.log(messageReaction));
+client.on("messageReactionAdd", (messageReaction) => console.log(messageReaction.user));
 
 
 client.login(process.env.token);
