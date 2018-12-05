e91ab4a79276e194f3d140808e522ca5cc670834
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-05 20:25:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -79,5 +79,18 @@ console.log("Az aktuális Raid-re jelentkezők száma : ", redcount );
   }
 });
 
+client.on('message', function(message) {
+    if (message.content == "^clear") {
+        if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.fetchMessages()
+               .then(function(list){
+                    message.channel.bulkDelete(list);
+                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
+        }
+    }
+
+});
+
+
 
 client.login(process.env.token);
