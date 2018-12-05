e8e1c6066ec8efef432689ed7451f36873e3670d
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-05 13:42:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.on("message", (message) => {
 client.on("messageReactionAdd", (messageReaction, user, message) => {
   if(messageReaction.emoji.name === "👍") {
 
-console.log(messageReaction.count);
+console.log(`Az aktuális raidre jelentkezők száma :  ${messageReaction.count}`);
   
   }
 });
