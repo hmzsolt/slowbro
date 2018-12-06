c634bf2eea75f07128cd4a526a52f222b3185f40
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-06 20:51:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -71,9 +71,13 @@ client.on("message", (message) => {
 client.on("messageReactionAdd", (messageReaction, message) => {
   if(messageReaction.emoji.name === "👍") {
 
-	  let redcountbot = messageReaction.count;
-	  const redcount = redcountbot-1;
-console.log("Az aktuális Raid-re jelentkezők száma : ", redcount );
+	  //let redcountbot = messageReaction.count;
+	 //const redcount = redcountbot-1;
+	  function raidcount() {
+  let redcountbot = messageReaction.count; 
+		  let redcount = redcountbot-1;
+}
+console.log("Az aktuális Raid-re jelentkezők száma : ",  ); return raidcount();
 	  //message.channel.send(`Az aktuális Raid-re jelentkezők száma : ${redcount}`);
 	    
   }
