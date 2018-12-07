ad30b0fb4ab78042394e180b43dba82ed7f032a1
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-07 08:40:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -78,6 +78,7 @@ client.on("messageReactionAdd", (messageReaction, message) => {
 		 // let redcount = redcountbot-1;
 //}
 console.log("Az aktuális Raid-re jelentkezők száma : ", redcount ); 
+	  console.log(messageReaction.users);
 	  //message.channel.send(`Az aktuális Raid-re jelentkezők száma : ${redcount}`);
 	    
   }
