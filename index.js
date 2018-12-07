c4346964c98303848e5d7a447c80933dfdce67f8
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-07 11:24:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ client.on("message", (message) => {
     message.channel.send(message.content.slice(5)).then(function (message) {
               message.react(jelentkezem);
               message.react(nemjelentkezem);
-	    const reactions = message.react(reaction => reaction.emoji.name === jelentkezem, {time: 15000});
+	    const reactions = message.react(reaction => reaction.emoji.name === jelentkezem);
 //message.channel.send(`${reactions.get(jelentkezem).count-1}`);
 		console.log(`Az aktuális Raid-re jelentkezők száma :  ${reactions.get(jelentkezem).count-1}`);	      
             }).catch(function() {
