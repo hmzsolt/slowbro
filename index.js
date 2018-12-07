530b4e0ffb5e1716db911b7b2b4a819001f6a24b
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-07 13:07:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.on('messageReactionAdd', (reaction, user) => {
     if(reaction.emoji.name === jelentkezem) {
 	  var  jelentkezok = reaction.count-1
         console.log(jelentkezok);
-	    message.channel.send(jelentkezok);
+	    
     }
 });
 
