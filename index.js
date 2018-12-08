f828843653b2ba05e8a85a63297c0cf081e0cdcf
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-08 20:34:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -72,12 +72,16 @@ client.on('messageReactionAdd', (reaction, user, message) => {
 	//user.send("Az aktuális Raid-re jelentkezők száma : ");
 	//user.send(jelentkezok);
 	
-	console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
+	/*console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
 	console.log("Az aktuális Raid-re jelentkezők száma : ");
-	console.log(jelentkezok);   
+	console.log(jelentkezok);  */ 
 	  
     }	
-	
+	if (jelentkezok > 1) {
+	console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
+	console.log("Az aktuális Raid-re jelentkezők száma : ");
+	console.log(jelentkezok);	
+	}
 	//message.channel.send("Az aktuális Raid-re jelentkezők száma :", `${jelentkezok}`);
 	
 	
