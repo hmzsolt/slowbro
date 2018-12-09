e512f2174565a82ff21683554c9035dbe37d5237
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-09 20:55:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ client.on("message", (message) => {
 var  jelentkezok = 0;
 client.on('messageReactionAdd', (reaction, user, message) => {
     if(reaction.emoji.name === jelentkezem && user.username != 'Slowbro' ) {
-	  var  jelentkezok = reaction.count;
+	  var  jelentkezok = reaction.count-1;
 	//user.send("Az aktuális Raid-re jelentkezők száma : ");
 	//user.send(jelentkezok);
 	
@@ -82,7 +82,7 @@ client.on('messageReactionAdd', (reaction, user, message) => {
 
 client.on('messageReactionRemove', (reaction, user, message) => {
     if(reaction.emoji.name === jelentkezem && user.username != 'Slowbro' ) {
-	  var  jelentkezok = reaction.count;
+	  var  jelentkezok = reaction.count-1;
 		
 	console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
 	console.log(`Az aktuális Raid-re jelentkezők száma : *** ${jelentkezok} fő. ***`);
