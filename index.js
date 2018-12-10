7a62a8571527ceecaeca3672115e1c511426f7f7
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-10 15:05:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ client.on('ready', () => {
 client.user.setActivity("Pokemon Go")
   console.log(`Logged in as ${client.user.tag} on ${client.guilds.size} server(s)!`);
 	console.log(`ID: ${client.user.id}`);
+	console.log(server.roles.get("name", "valor"));
+	console.log(server.roles.get("name", "mystic"));
+	console.log(server.roles.get("name", "instinct"));
 
  /* console.log("Servers:");
     client.guilds.forEach((guild) => {
