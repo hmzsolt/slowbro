f179ae489d52f3462e49af8cb6061b938bf95aee
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-10 09:03:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.on('messageReactionAdd', (reaction, user, message) => {
 	  var  jelentkezok = reaction.count-1;
 	//user.send("Az aktuális Raid-re jelentkezők száma : ");
 	//user.send(jelentkezok);
-	message.reply('you reacted with a thumbs up.');
+	
 	console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
 	console.log(`Az aktuális Raid-re jelentkezők száma : *** ${jelentkezok} fő. ***`);
 	    
