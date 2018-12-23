e177e8861b47bc95ddb7e8b78873333e3399b134
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-23 19:18:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -154,13 +154,94 @@ fs.readdir('./commands/', async (err, files) => {
   });*/
 
 const prefix_team = "!!";
-client.on('message', (message) => {
+client.on('message', async message => {
   if (!message.content.startsWith(prefix_team) || message.author.bot) return;
  
   if (message.content.startsWith(prefix_team + "team")) {
-	  
-	};
-});  
+     await message.delete().catch(O_o=>{});
+
+    const a = message.guild.roles.get('458317524581351426'); // valor
+    const b = message.guild.roles.get('458610003482509322'); // mystic
+    const c = message.guild.roles.get('458611006508105728'); // instinct
+
+
+    const filter = (reaction, user) => ['🇻', '🇲', '🇮'].includes(reaction.emoji.name) && user.id === message.author.id;
+
+    const embed = new Discord.RichEmbed()
+        .setTitle('Választható csapatok:')
+        .setThumbnail('https://raw.githubusercontent.com/hmzsolt/slowbro/master/f2vxtZYdIU3C_ZWZkoBorqacLg6NM8f3hAJ_UqDDr-0.png')
+        .setDescription(`
+                
+        🇻 Valor ${a.toString()}
+
+        🇲 Mystic ${b.toString()}
+
+        🇮 Instinct ${c.toString()}
+        `)
+        .setColor(0xdd9323)
+        .setFooter(`ID: ${message.author.username}`);
+        
+    message.channel.send(embed).then(async msg => {
+
+        await msg.react('🇻');
+        await msg.react('🇲');
+        await msg.react('🇮');
+
+        msg.awaitReactions(filter, {
+            max: 1,
+            time: 30000,
+            errors: ['time']
+        }).then(collected => {
+
+            const reaction = collected.first();
+
+            switch (reaction.emoji.name) {
+                case '🇻':
+                    if (message.member.roles.has(a.id)) {
+                        msg.delete(2000);
+                        return message.channel.send('Van már ilyen rangod!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(a).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Hiba: **${err.message}**.`);
+                    });
+                    message.channel.send(`Sikeresen hozzá lettél adva a **${a.name}** csapathoz!`).then(m => m.delete(3000));
+                    msg.delete();
+                    break;
+                case '🇲':
+                    if (message.member.roles.has(b.id)) {
+                        msg.delete(2000);
+                        return message.channel.send('Van már ilyen rangod!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(b).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Hiba: **${err.message}**.`);
+                    });
+                    message.channel.send(`Sikeresen hozzá lettél adva a **${b.name}** csapathoz!`).then(m => m.delete(3000));
+                    msg.delete();
+                    break;
+                case '🇮':
+                    if (message.member.roles.has(c.id)) {
+                        msg.delete(2000);
+                        return message.channel.send('Van már ilyen rangod!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(c).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Hiba: **${err.message}**.`);
+                    });
+                    message.channel.send(`Sikeresen hozzá lettél adva a **${c.name}** csapathoz!`).then(m => m.delete(3000));
+                    msg.delete();
+                    break;
+            }
+        }).catch(collected => {
+            return message.channel.send(`Nem választottál csapatot !`).then(m => m.delete(3000));
+        });
+
+    });
+    
+  }
+  
+});   
 
 client.login(process.env.token);
 
