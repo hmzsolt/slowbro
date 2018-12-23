48acebb9678d26890af7fce46148e452481d3f4f
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-23 17:08:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -129,5 +129,29 @@ client.on('messageReactionRemove', (reaction, user, message) => {
 
 });*/
 
+const fs = require("fs");
+
+fs.readdir('./events/', (err, files) => {
+  if (err) return console.error;
+  files.forEach(file => {
+    if (!file.endsWith('.js')) return;
+    const evt = require(`./events/${file}`);
+    let evtName = file.split('.')[0];
+    console.log(`Loaded event '${evtName}'`);
+    client.on(evtName, evt.bind(null, client));
+  });
+});
+
+fs.readdir('./commands/', async (err, files) => {
+    if (err) return console.error;
+    files.forEach(file => {
+      if (!file.endsWith('.js')) return;
+      let props = require(`./commands/${file}`);
+      let cmdName = file.split('.')[0];
+      console.log(`Loaded command '${cmdName}'`);
+      client.commands.set(cmdName, props);
+    });
+  });
+
 client.login(process.env.token);
 
