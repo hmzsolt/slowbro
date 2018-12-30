5106733cfa6e00399a73dbf21f9645a67f25ebb8
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-30 16:36:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,9 @@ client.on('messageReactionAdd', (reaction, user) => {
     
     console.log(`${user.username} reacted with "${reaction.emoji.name}". Az aktuális Raid-re jelentkezők száma : *** ${jelentkezok} fő. ***`);
     
-    var users = [user.username];
+    var users = [user.username]
+    users.push(user.username);
+	    
     console.log(users);
 	    
     const channel = client.channels.find(channel => channel.name === 'reports'); // red🔴
