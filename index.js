d62e8bd0de473ffdb964a5677eaee39ceea5b17f
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-31 20:06:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -87,15 +87,13 @@ client.on('messageReactionAdd', (reaction, user) => {
     if(reaction.emoji.name === jelentkezem && user.username != 'Slowbro' ) {
 	var  jelentkezok = reaction.count-1;
     
-    var users = [];
-    users.push(user.username);
-    var userss = users.concat(users);
-    
-    console.log(userss);	    
+    let fetchedUsers = reaction.fetchUsers();
+    fetchedUsers = fetchedUsers.array();
+    console.log(fetchedUsers);	    
 	    
     console.log(`${user.username} reacted with "${reaction.emoji.name}". Az aktuális Raid-re jelentkezők száma : *** ${jelentkezok} fő. ***`);
         	    
-    const channel = client.channels.find(channel => channel.name === 'red🔴'); // red🔴
+    const channel = client.channels.find(channel => channel.name === 'reports'); // red🔴
         
     //channel.send(`Az aktuális Raid-re jelentkezők száma : *** ${jelentkezok} fő. ***`);  //red 458620540555493376
     
