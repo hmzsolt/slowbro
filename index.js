82b1b62e862a0e22376050084e31c659a7c1594a
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2018-12-31 13:03:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -138,9 +138,9 @@ client.on('message', async message => {
 	if (message.content.startsWith(prefix_clear + "clear")) {
 	var args = message.content.split(" ");
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-    if(!args[0]) return message.channel.send("no");
-    message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+    if(!args[1]) return message.channel.send("no");
+    message.channel.bulkDelete(args[1]).then(() => {
+    message.channel.send(`Clear ${args[1]} messages.`).then(msg => msg.delete(2000));
     
       });
         
