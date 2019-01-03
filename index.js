9d3585dc82496b1aff0d68e4ad9938e824dd01e0
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2019-01-03 23:06:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -357,7 +357,7 @@ if (message.content.startsWith(prefix_tempmute + "tempmute")){
 
   let muteembed = new Discord.RichEmbed()
   .setDescription(`Mute executed by ${message.author}`)
-  .setColor(#ffa500)
+  .setColor(0xdd9323)
   .addField("Muted User", tomute)
   .addField("Muted in", message.channel)
   .addField("Time", message.createdAt)
