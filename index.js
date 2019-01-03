dca598d1e0de561b6fcd0bd64e20150a8550bb89
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2019-01-03 22:57:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -309,6 +309,76 @@ client.on('guildMemberAdd', member => {
 `);
 	
 });
+const prefix_tempmute = "!!";
+client.on('message', async message => {
+
+if (!message.content.startsWith(prefix_tempmute) || message.author.bot) return;    
+
+if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No can do.");
+
+if (message.content.startsWith(prefix + "tempmute")){
+
+  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
+  if(!tomute) return message.reply("Couldn't find user.");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+  //let reason = args.slice(2).join(" ");
+  //if(!reason) return message.reply("Please supply a reason.");
+
+  let muterole = message.guild.roles.find(`name`, "muted");
+  //start of create role
+  if(!muterole){
+    try{
+      muterole = await message.guild.createRole({
+        name: "muted",
+        color: "#000000",
+        permissions:[]
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(muterole, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false
+        });
+      });
+    }catch(e){
+      console.log(e.stack);
+    }
+  }
+  //end of create role
+  let mutetime = args[1];
+  if(!mutetime) return message.reply("You didn't specify a time!");
+
+  message.delete().catch(O_o=>{});
+
+  try{
+    await tomute.send(`Hi! You've been muted for ${mutetime}. Sorry!`)
+  }catch(e){
+    message.channel.send(`A user has been muted... but their DMs are locked. They will be muted for ${mutetime}`)
+  }
+
+  let muteembed = new Discord.RichEmbed()
+  .setDescription(`Mute executed by ${message.author}`)
+  .setColor(orange)
+  .addField("Muted User", tomute)
+  .addField("Muted in", message.channel)
+  .addField("Time", message.createdAt)
+  .addField("Length", mutetime)
+  //.addField("Reason", reason);
+
+  let reportschannel = message.guild.channels.find(`name`, "reports");
+  if(!reportschannel) return message.reply("Please create a reports channel first!");
+  reportschannel.send(muteembed);
+
+  await(tomute.addRole(muterole.id));
+
+  setTimeout(function(){
+    tomute.removeRole(muterole.id);
+    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+  }, ms(mutetime));
+
+
+//end of module
+}
+});
 
 client.login(process.env.token);
 
