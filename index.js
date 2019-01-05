fddbbbed7e6b735ef677741776c4ef44e10596af
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2019-01-05 11:24:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const jelentkezem = "🖖";
+const nemjelentkezem = "👎";
+const prefix = "!";
+
 
 function catchErr (err, message) {
     client.user.get('447676992431849483').send("There was an error at channel " + message.channel + " in guild " + message.guild);
@@ -12,19 +16,7 @@ client.on('ready', () => {
 client.user.setActivity("b0t")
   console.log(`Logged in as ${client.user.tag} on ${client.guilds.size} server(s)!`);
 	console.log(`ID: ${client.user.id}`);
-
-/*  console.log("Servers:");
-    client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name)
-					
-	guild.roles.forEach((role) => {
-		console.log(` -- ${role.name} - ${role.id}`)
-					
-  	guild.channels.forEach((channel) => {
-            console.log(` --- ${channel.name} - (${channel.type}) - ${channel.id}`)
-		})        
-	})
-    }) */   
+   
 });
 
 client.on('message', async msg => {
@@ -36,24 +28,20 @@ client.on('message', async msg => {
 	"Jogosulatlan előnyszerzés miatt transferelned kell.",
 	"Ugye ezt nem gondoltad komolyan?!",
 	"2 perced van felhúzni lvl 40-ig, vagy transferelned kell!",
-	"Ezt most megtarthatod..." 
+    "Ezt most megtarthatod...",
+    "Ez ugye csak valami rossz vicc akart lenni?" 
 ];
 var response = helloResponses[Math.floor(Math.random()*helloResponses.length)];
   if (msg.attachments.size > 0 && msg.channel.id === '504535266069970945') {
     msg.reply(response).then().catch(console.error);
-  
-	  
   }  
 	  
-  if (msg.attachments.size > 0 && msg.channel.id === '458620540555493376')   {
+  if (msg.attachments.size > 0 && msg.channel.id === '458620540555493376') {
     msg.delete(1);
     msg.reply("Kérlek, szöveges formátumban jelentsd a raidet. Pl.: !red GYM neve - reccsenés időpontja - tier").then(m => m.delete(60000));
   }
-	    
 });
-const jelentkezem = "🖖";
-const nemjelentkezem = "👎";
-const prefix = "!";
+
 client.on('message', (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
  
@@ -63,11 +51,9 @@ client.on('message', (message) => {
     message.react(jelentkezem);
     //message.react(nemjelentkezem);
     message.channel.send(`Az aktuális Raid-re jelentkezők száma : *** 0 fő. ***`);	    
-    
-	       
-            }).catch(function() {
+    }).catch(function() {
               //Something
-             });
+    });
   }
 		
 });
@@ -85,9 +71,7 @@ client.on('message', (message) => {
       .addField("Gym neve: ", args[1])
       .addField("Reccsenés időpontja: ", args[2])
       .addField("Tier / Boss: ", args[3]);
-      //.addField("Length", mutetime)
-      //.addField("Reason", reason);
-
+      
       message.delete(1).catch();
       message.channel.send(`Trainerek figyelem! @everyone @here !`);
       message.channel.send(raidembed).then(function (message) {
@@ -138,12 +122,12 @@ client.on('messageReactionRemove', (reaction, user) => {
     }
 });	
 
-const prefix_say = "!!";
 client.on('message', async message => {
     
-	if (!message.content.startsWith(prefix_say) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Nincs hozzá jogosultságod.");
 	
-	if (message.content.startsWith(prefix_say + "say")) {
+	if (message.content.startsWith(prefix + "say")) {
 	
      await message.delete().catch(O_o=>{});
     var args = message.content.split(" ");
@@ -152,115 +136,6 @@ client.on('message', async message => {
 	}	
 });
 
-/*const prefix_clear = "!!";
-client.on('message', async message => {
-    
-	try {
-	
-    if (!message.content.startsWith(prefix_clear) || message.author.bot) return;
-	
-    if (message.content.startsWith(prefix_clear + "clear")) {
-    var args = message.content.split(" ");
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-    if(!args[1]) return message.channel.send("no");
-    message.channel.bulkDelete(args[1]).then(() => {
-    message.channel.send(`***${args[1]} üzenet törölve.***`).then(msg => msg.delete(2000)).catch(function() { });
-    
-      });
-        
-	}
-		}
-catch (err){
-    catchErr(err, message)
-}
-});*/
-
-/*const prefix_team = "!!";
-client.on('message', async message => {
-  if (!message.content.startsWith(prefix_team) || message.author.bot) return;
- 
-  if (message.content.startsWith(prefix_team + "team") && message.channel.id === '458617725770661898') {
-    message.delete();
-    const a = message.guild.roles.get('458317524581351426'); // valor
-    const b = message.guild.roles.get('458610003482509322'); // mystic
-    const c = message.guild.roles.get('458611006508105728'); // instinct
-
-    const filter = (reaction, user) => ['🇻', '🇲', '🇮'].includes(reaction.emoji.name) && user.id === message.author.id;
-
-    const embed = new Discord.RichEmbed()
-        .setTitle('Választható csapatok:')
-        .setThumbnail(client.user.displayAvatarURL)
-        .setDescription(`
-                
-        🇻 Valor ${a.toString()}
-        🇲 Mystic ${b.toString()}
-        🇮 Instinct ${c.toString()}
-        `)
-        .setColor(0xdd9323)
-        .setFooter(`ID: ${message.author.username}`);
-        
-    message.channel.send(embed).then(async msg => {
-
-        await msg.react('🇻');
-        await msg.react('🇲');
-        await msg.react('🇮');
-
-        msg.awaitReactions(filter, {
-            max: 1,
-            time: 30000,
-            errors: ['time']
-        }).then(collected => {
-
-            const reaction = collected.first();
-
-            switch (reaction.emoji.name) {
-                case '🇻':
-                    if (message.member.roles.has(a.id)) {
-                        msg.delete(2000);
-                        return message.channel.send('Van már ilyen rangod!').then(m => m.delete(3000));
-                    }
-                    message.member.addRole(a).catch(err => {
-                        console.log(err);
-                        return message.channel.send(`Hiba: **${err.message}**.`);
-                    });
-                    message.channel.send(`Sikeresen hozzá lettél adva a **${a.name}** csapathoz!`).then(m => m.delete(3000));
-                    msg.delete();
-                    break;
-                case '🇲':
-                    if (message.member.roles.has(b.id)) {
-                        msg.delete(2000);
-                        return message.channel.send('Van már ilyen rangod!').then(m => m.delete(3000));
-                    }
-                    message.member.addRole(b).catch(err => {
-                        console.log(err);
-                        return message.channel.send(`Hiba: **${err.message}**.`);
-                    });
-                    message.channel.send(`Sikeresen hozzá lettél adva a **${b.name}** csapathoz!`).then(m => m.delete(3000));
-                    msg.delete();
-                    break;
-                case '🇮':
-                    if (message.member.roles.has(c.id)) {
-                        msg.delete(2000);
-                        return message.channel.send('Van már ilyen rangod!').then(m => m.delete(3000));
-                    }
-                    message.member.addRole(c).catch(err => {
-                        console.log(err);
-                        return message.channel.send(`Hiba: **${err.message}**.`);
-                    });
-                    message.channel.send(`Sikeresen hozzá lettél adva a **${c.name}** csapathoz!`).then(m => m.delete(3000));
-                    msg.delete();
-                    break;
-            }
-        }).catch(collected => {
-		msg.delete(2000);
-        return message.channel.send(`Nem választottál csapatot !`).then(m => m.delete(3000));
-        });
-	
-    });
-
-  }
-  
-}); */
 
 const prefix_team = "!!";
 client.on('message', async message => {
@@ -340,23 +215,22 @@ client.on('guildMemberAdd', member => {
 	
 });
 
-const prefix_tempmute = "!!";
 client.on('message', async message => {
 
-if (!message.content.startsWith(prefix_tempmute) || message.author.bot) return;    
+if (!message.content.startsWith(prefix) || message.author.bot) return;    
 
-if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No can do.");
+if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Nincs hozzá jogosultságod.");
 
-if (message.content.startsWith(prefix_tempmute + "tempmute")){
+if (message.content.startsWith(prefix + "tempmute")){
   var args = message.content.split(" ");
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
-  if(!tomute) return message.reply("Couldn't find user.");
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+  if(!tomute) return message.reply("Nincs ilyen felhasználó.");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Őt nem lehet némítani!");
   //let reason = args.slice(2).join(" ");
   //if(!reason) return message.reply("Please supply a reason.");
 
-  let muterole = message.guild.roles.find(`name`, "muted");
- /* //start of create role
+  /* let muterole = message.guild.roles.find(`name`, "muted");
+  //start of create role
   if(!muterole){
     try{
       muterole = await message.guild.createRole({
@@ -374,9 +248,9 @@ if (message.content.startsWith(prefix_tempmute + "tempmute")){
       console.log(e.stack);
     }
   }
-  //end of create role*/
+  //end of create role */
   let mutetime = args[2];
-  if(!mutetime) return message.reply("You didn't specify a time!");
+  if(!mutetime) return message.reply("Nem adtál meg időt.");
 
   message.delete().catch(O_o=>{});
 
@@ -387,14 +261,13 @@ if (message.content.startsWith(prefix_tempmute + "tempmute")){
   }
 
   let muteembed = new Discord.RichEmbed()
-  .setDescription(`Mute executed by ${message.author}`)
+  .setDescription(`Némító: ${message.author}`)
   .setColor(0xdd9323)
-  .addField("Muted User", tomute)
-  .addField("Muted in", message.channel)
-  .addField("Time", message.createdAt)
-  .addField("Length", mutetime)
-  //.addField("Reason", reason);
-
+  .addField("Némított felhasználó", tomute)
+  .addField("Hol lett némítva", message.channel)
+  .addField("Némítás ideje", message.createdAt)
+  .addField("Némítás hossza", mutetime);
+  
   let reportschannel = message.guild.channels.find(channel => channel.name === 'reports');
   if(!reportschannel) return message.reply("Please create a reports channel first!");
   reportschannel.send(muteembed);
@@ -403,7 +276,7 @@ if (message.content.startsWith(prefix_tempmute + "tempmute")){
 
   setTimeout(function(){
     tomute.removeRole(muterole.id);
-    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    message.channel.send(`<@${tomute.id}> némítása feloldva!`);
   }, (60000*(mutetime)));
 
 
@@ -412,4 +285,3 @@ if (message.content.startsWith(prefix_tempmute + "tempmute")){
 });
 
 client.login(process.env.token);
-
