3d76f0b3bd47e7d51dafe9d9321409ddf3b9cabd
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2019-01-10 07:58:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -139,11 +139,32 @@ client.on('message', async message => {
     console.log(args);
 		console.log(args[1].length);
 		
-    if(args[1] === '<#514574722495676417>') { const channel_say =client.channels.get('514574722495676417'); 
-    message.channel.send(message.content.slice(5));
-		channel_say.send(message.content.slice(5+args[1].length));
-	}
-	}	
+    if(args[1] === '<#514574722495676417>') { //silph 
+        const channel_say =client.channels.get('514574722495676417'); 
+    	channel_say.send(message.content.slice(5+args[1].length));
+    }
+    else if(args[1] === '<#504535266069970945>') { //ekuki
+        const channel_say =client.channels.get('504535266069970945'); 
+    	channel_say.send(message.content.slice(5+args[1].length));
+    }
+    else if(args[1] === '<#459575823805054976>') { //barát
+        const channel_say =client.channels.get('459575823805054976'); 
+    	channel_say.send(message.content.slice(5+args[1].length)); 
+    }
+    else if(args[1] === '<#458620540555493376>') { //red
+        const channel_say =client.channels.get('458620540555493376'); 
+        channel_say.send(message.content.slice(5+args[1].length));
+    }
+    else if(args[1] === '<#474077389186400276>') { //hirek
+        const channel_say =client.channels.get('474077389186400276'); 
+        channel_say.send(message.content.slice(5+args[1].length));
+    }
+    else if(args[1] === '<#458620489540304929>') { //chat
+        const channel_say =client.channels.get('458620489540304929'); 
+        channel_say.send(message.content.slice(5+args[1].length));
+    }
+    else message.channel.send(message.content.slice(5));
+}        	
 });
 
 
