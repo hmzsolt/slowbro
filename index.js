822936dc02936734bcca173379ba3949bad6374b
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2019-01-11 11:02:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -355,21 +355,16 @@ client.on('message', async message => {
     
   if (message.content.startsWith(prefix + "counters")){
       message.delete().catch(O_o=>{});
-      
-
-    try{
+    
       var args = message.content.split(" ");
       console.log(args);
       console.log(args[1]);
-      
-    
-      message.channel.send(`https://www.pokebattler.com/raids/defenders/${args[1].toUpperCase()}/levels/RAID_LEVEL_5/attackers/levels/40/strategies/CINEMATIC_ATTACK_WHEN_POSSIBLE/DEFENSE_RANDOM_MC?sort=OVERALL&weatherCondition=NO_WEATHER&dodgeStrategy=DODGE_REACTION_TIME&aggregation=AVERAGE`);
-    
-    }catch
-    {
+     
       if (args[1].size < 1) return message.channel.send(`Nem adtál meg Tier Bosst.`);
-    }
-
+      
+      message.channel.send(`https://www.pokebattler.com/raids/defenders/${args[1].toUpperCase()}/levels/RAID_LEVEL_5/attackers/levels/40/strategies/CINEMATIC_ATTACK_WHEN_POSSIBLE/DEFENSE_RANDOM_MC?sort=OVERALL&weatherCondition=NO_WEATHER&dodgeStrategy=DODGE_REACTION_TIME&aggregation=AVERAGE&randomAssistants=-1`);
+     
+    
     }
   });
 
