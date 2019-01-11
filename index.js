8295e34ca9dbfd3b52bdcc93728086a9abdc1800
hmzsolt <40390317+hmzsolt@users.noreply.github.com>
2019-01-11 11:06:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -358,10 +358,12 @@ client.on('message', async message => {
     
       var args = message.content.split(" ");
       console.log(args);
-      console.log(args[1]);
-     
-      if (args[1].length < 1) return message.channel.send(`Nem adtál meg Tier Bosst.`);
       
+      let boss = args.slice(1).join(' ');
+      console.log(boss);
+     
+      if (boss.length < 1) return message.channel.send(`Nem adtál meg Tier Bosst.`);
+
       message.channel.send(`https://www.pokebattler.com/raids/defenders/${args[1].toUpperCase()}/levels/RAID_LEVEL_5/attackers/levels/40/strategies/CINEMATIC_ATTACK_WHEN_POSSIBLE/DEFENSE_RANDOM_MC?sort=OVERALL&weatherCondition=NO_WEATHER&dodgeStrategy=DODGE_REACTION_TIME&aggregation=AVERAGE&randomAssistants=-1`);
      
     
